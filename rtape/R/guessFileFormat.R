ba3dec68574dd25af82b045a56a5a1c0d96385ba
Miron B. Kursa <M.Kursa@icm.edu.pl>
2011-05-10 23:22:01 +0200

    Redundant debugprintf removed.

--- rtape/R/guessFileFormat.R
+++ rtape/R/guessFileFormat.R
@@ -10,7 +10,6 @@ guessFileFormat<-function(fName){
  stopifnot(length(fName)==1)
  if(!file.exists(fName)) return(makeFileFormat())
  readBin(fName,what="int",n=6,size=1,signed=FALSE)->header
- print(header);
  if(identical(header[1:2],c(0x1fL,0x8bL))) return(makeFileFormat("gz"))
  if(identical(header[1:6],c(0xfdL,0x37L,0x7aL,0x58L,0x5aL,0x00L))) return(makeFileFormat("xz"))
  if(identical(header[1:3],c(0x42L,0x5aL,0x68L))) return(makeFileFormat("bz"))
