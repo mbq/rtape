8aaa4d22356d004bb0aac8d7a448c046331ec303
Miron B. Kursa <M.Kursa@icm.edu.pl>
2012-03-27 12:16:14 +0200

    Upgrade of building script.

--- makePkg.sh
+++ makePkg.sh
@@ -4,11 +4,11 @@ rm -rf readyPkg/
 mkdir readyPkg
 mkdir readyPkg/rtape/
 cp rtape/DESCRIPTION readyPkg/rtape/.
-cp rtape/NAMESPACE readyPkg/rtape/.
 mkdir readyPkg/rtape/R
 cp rtape/R/*.R readyPkg/rtape/R/.
 cd readyPkg
-R CMD roxygen -d rtape
+Rscript -e "require(roxygen);roxygenize('rtape','rtape',copy=FALSE)"
+cp ../rtape/NAMESPACE rtape/.
 R CMD build rtape
 cd ..
 
