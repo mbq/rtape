e0efcb962f3c8fe74bcb78d5cb63bbe3e4a6d9d1
Miron B. Kursa <M.Kursa@icm.edu.pl>
2012-03-27 12:27:14 +0200

    NEWS and connected update of the build script.

--- makePkg.sh
+++ makePkg.sh
@@ -6,9 +6,12 @@ mkdir readyPkg/rtape/
 cp rtape/DESCRIPTION readyPkg/rtape/.
 mkdir readyPkg/rtape/R
 cp rtape/R/*.R readyPkg/rtape/R/.
+mkdir readyPkg/rtape/inst
+cp rtape/inst/NEWS readyPkg/rtape/inst/NEWS
 cd readyPkg
 Rscript -e "require(roxygen);roxygenize('rtape','rtape',copy=FALSE)"
 cp ../rtape/NAMESPACE rtape/.
 R CMD build rtape
+R CMD check rtape*.tar.gz
 cd ..
 
